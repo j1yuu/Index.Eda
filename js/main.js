a41443bf92fc3bc4d2004b006e66178436532927
j1yuu <59069015+j1yuu@users.noreply.github.com>
2022-02-10 23:03:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // direction: 'vertical',
+    loop: true,
+
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    }
+});
